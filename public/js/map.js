1f0c5c636343f1a66eae47590182e22029fe5388
Sagar Vashist <sagarvashist02@gmail.com>
2026-08-01 02:23:44 +0530

    Fix MapLibre initialization using global maplibregl script

--- public/js/map.js
+++ public/js/map.js
@@ -1,31 +1,28 @@
-import {
-  Map,
-  NavigationControl,
-  Marker,
-  Popup,
-} from "https://cdn.skypack.dev/maplibre-gl";
-
 var myAPIKey = (typeof mapToken !== "undefined" && mapToken) ? mapToken : "91a747defc6d451299a4bc539e931e81";
 
 var mapCoords = (typeof coordinates !== "undefined" && Array.isArray(coordinates) && coordinates.length === 2 && (coordinates[0] !== 0 || coordinates[1] !== 0))
   ? coordinates
   : [77.2090, 28.6139];
 
-var map = new Map({
-  container: "my-map",
-  style: `https://maps.geoapify.com/v1/styles/osm-carto/style.json?apiKey=${myAPIKey}`,
-  center: mapCoords,
-  zoom: 9,
-});
+if (typeof maplibregl !== "undefined") {
+  var map = new maplibregl.Map({
+    container: "my-map",
+    style: `https://maps.geoapify.com/v1/styles/osm-carto/style.json?apiKey=${myAPIKey}`,
+    center: mapCoords,
+    zoom: 9,
+  });
 
-map.addControl(new NavigationControl());
+  map.addControl(new maplibregl.NavigationControl());
 
-const popup = new Popup({ offset: 25 }).setHTML(`
-    <h6>Welcome to WanderLust!</h6>
-    <p>Exact location will be provided after booking.</p>
-`);
+  const popup = new maplibregl.Popup({ offset: 25 }).setHTML(`
+      <h6>Welcome to WanderLust!</h6>
+      <p>Exact location will be provided after booking.</p>
+  `);
 
-const marker = new Marker({ color: "red" })
-  .setLngLat(mapCoords)
-  .setPopup(popup)
-  .addTo(map);
+  const marker = new maplibregl.Marker({ color: "red" })
+    .setLngLat(mapCoords)
+    .setPopup(popup)
+    .addTo(map);
+} else {
+  console.error("maplibregl library is not loaded.");
+}
